22c137af779de7ab859c470c7692330617020813
Forthtilliath <forth@live.fr>
2023-11-02 02:32:45 +0100

    Remove useless types

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,16 +1,6 @@
 import { ComponentPropsWithoutRef } from "react";
 
 declare global {
-  declare module "*.mp4" {
-    const src: string;
-    export default src;
-  }
-
-  declare module "*.svg" {
-    const src: string;
-    export default src;
-  }
-
   type StyledProps<
     Tag extends keyof HTMLElementTagNameMap,
     Props = {}
